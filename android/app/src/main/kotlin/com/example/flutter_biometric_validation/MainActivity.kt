c9f871dddf1b9b0a8a74c856c16ed5bf5a4636dc
zeenayouhan <zinayouhan33@gmail.com>
2022-02-23 08:14:14 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_biometric_validation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_biometric_validation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_biometric_validation/faceid_screen.dart';
+
+
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const String title = 'Local Auth';
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.purple),
+        home: FaceIdPage(),
+      );
+}
